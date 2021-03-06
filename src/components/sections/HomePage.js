6999e63b3cfcff7e53c985fa8d9e3fe7a1c84d0c
Satyanchal <62104921+Bluehatcoders@users.noreply.github.com>
2021-03-07 00:21:42 +0530

    Update HomePage.js

--- src/components/sections/HomePage.js
+++ src/components/sections/HomePage.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
+const HomePage = () => {
   return (
     <>
      <iframe title="Crytpodex" src="https://crypto-dex.netlify.app/" width="540" height="450"></iframe>
     </>
   );
+  };
 
 export default HomePage;
